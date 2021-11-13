b5b2e1d83d7c913295879eb3a34129c19ee8a3f3
Sourav ghosh <souravghosh1226@gmail.com>
2021-11-13 14:54:19 +0530

    set up user

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -9,7 +9,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>{{('app.name','Laravel')}} -@yield('title')</title>
+   
+    <title>{{ config('app.name', 'Project') }} - @yield('title')</title>
+
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
