a88f5a5f0c3396895808a20af359bcc2ef41fc1a
Sourav ghosh <souravghosh1226@gmail.com>
2021-11-13 13:57:12 +0530

    set up user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    {{-- <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-    </div>
+    </div> --}}
 
     <!-- Content Row -->
     <div class="row">
@@ -99,7 +99,7 @@
 
     <!-- Content Row -->
 
-    <div class="row">
+    {{-- <div class="row">
 
         <!-- Area Chart -->
         <div class="col-xl-8 col-lg-7">
@@ -173,10 +173,10 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <!-- Content Row -->
-    <div class="row">
+    {{-- <div class="row">
 
         <!-- Content Column -->
         <div class="col-lg-6 mb-4">
@@ -326,7 +326,7 @@
             </div>
 
         </div>
-    </div>
+    </div> --}}
 
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/employee/list', [App\Http\Controllers\UserController::class, 'userList'])->name('list');
+Route::get('/employee/salary', [App\Http\Controllers\UserController::class, 'userSalary'])->name('salary');
